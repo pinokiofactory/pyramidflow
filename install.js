2fc676050796dc7b8ed6c5f20505a56c9a485b94
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-16 05:29:04 -0500

    change torch order

--- install.js
+++ install.js
@@ -20,6 +20,18 @@ module.exports = {
         ]
       }
     },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install gradio",
+          "pip install -r requirements.txt"
+        ]
+      }
+    },
     // Delete this step if your project does not use torch
     {
       when: "{{platform !== 'darwin'}}",
@@ -31,18 +43,6 @@ module.exports = {
           path: "app",                // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
         }
-      }
-    },
-    // Edit this step with your custom install commands
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install gradio devicetorch",
-          "pip install -r requirements.txt"
-        ]
       }
     },
     {
