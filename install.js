7a712748d7e07e06fb7f7ee47160732d1f838ed4
6Morpheus6 <wanton-decay@gmx.net>
2025-11-19 18:46:29 +0100

    update
    - add bundle ai
    - remove fs.link
    - remove comments

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 module.exports = {
+  requires: {
+    bundle: "ai"
+  },
   run: [
-    // Edit this step to customize the git repository to use
     {
       when: "{{platform !== 'darwin'}}",
       method: "shell.run",
@@ -19,65 +21,27 @@ module.exports = {
         ]
       }
     },
-//    {
-//      when: "{{platform === 'darwin'}}",
-//      method: "shell.run",
-//      params: {
-//        path: "app",
-//        message: [
-//          "git fetch --all",
-//          "git checkout use_mps_on_apple_silicon"
-//        ]
-//      }
-//    },
-//    {
-//      method: "shell.run",
-//      params: {
-//        message: [
-//          "git clone https://github.com/jy0205/Pyramid-Flow app",
-//        ]
-//      }
-//    },
-//    {
-//      when: "{{platform === 'darwin'}}",
-//      method: "shell.run",
-//      params: {
-//        path: "app",
-//        message: [
-//          "git fetch origin pull/113/head:use_mps_on_apple_silicon",
-//          "git switch use_mps_on_apple_silicon"
-//        ]
-//      }
-//    },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
           "pip install gradio",
           "pip install -r requirements.txt"
         ]
       }
     },
-    // Delete this step if your project does not use torch
     {
       when: "{{platform !== 'darwin'}}",
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
+          venv: "env",
+          path: "app",
+          // xformers: true
         }
-      }
-    },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
       }
     }
   ]
